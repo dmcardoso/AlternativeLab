b806a0a84f3609f4cc1ee3063b0f32aab4f048f9
Daniel Moreira <dmcardoso@github.com>
2018-10-25 10:48:28 -0300

    inicio do módulo de node

---
+++ ScriptsJS/WebModerno/cap_node/aula02/moduloA.js
@@ -0,0 +1,8 @@
+this.ola = "Fala Pessoa";
+
+//Linha para configurar o runtime do node no php storm
+// const path = require('path');
+
+exports.bemVindo = "Bem vindo ao node!";
+
+module.exports.ateLogo = "Até o próximo exemplo";
---
+++ ScriptsJS/WebModerno/cap_node/aula03/usandoModulosTerceiros.js
@@ -0,0 +1,6 @@
+//usar npm -i lodash <- antes de rodar
+const _ = require('lodash');
+
+setInterval(() => console.log(_.random(25, 50)), 2000);
+const a = 6;
+console.log(a);
