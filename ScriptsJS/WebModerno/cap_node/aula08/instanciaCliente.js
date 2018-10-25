d92d914a4ee6922479fcfd04988405b7ede24ea9
Daniel Moreira <dmcardoso@github.com>
2018-10-25 14:07:45 -0300

    aula 08

---
+++ ScriptsJS/WebModerno/cap_node/aula08/instanciaCliente.js
@@ -0,0 +1,16 @@
+const contadorA = require('./instanciaUnica');
+const contadorB = require('./instanciaUnica');
+
+const contadorC = require('./instanciaNova')();
+const contadorD = require('./instanciaNova')();
+
+contadorA.inc();
+contadorA.inc();
+
+console.log(contadorB.valor);
+
+contadorC.inc();
+contadorC.inc();
+
+console.log(contadorC.valor);
+console.log(contadorD.valor);
