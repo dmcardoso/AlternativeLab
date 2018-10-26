ffeed7190fe9ec1f065a157fa8fdb21823cb874b
dmcardoso <qualquercoisa6568@gmail.com>
2018-10-25 23:01:51 -0300

    aula 13 lendo arquivos

---
+++ ScriptsJS/WebModerno/cap_node/aula13/leituraarquivo.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+
+const caminho = __dirname + '/arquivo.json';
+
+//sincrono
+const conteudo = fs.readFileSync(caminho, "utf-8");
+
+console.log(conteudo);
+
+//assincrono
+fs.readFile(caminho, 'utf-8', (err, conteudo) => {
+    const config = JSON.parse(conteudo);
+    console.log(`${config.db.host}: ${config.db.port}`);
+});
+
+// Para JSON
+const config = require('./arquivo.json');
+console.log(config.db);
+
+fs.readdir(__dirname, (err, conteudo) => {
+    console.log("Arquivos da pasta");
+    console.log(conteudo);
+});
