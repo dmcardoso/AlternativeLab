fb38b8a1e7d241c02d9a51cfc5c77014f29cb393
Daniel Moreira <dmcardoso@github.com>
2018-11-12 15:21:20 -0200

    ajax

--- ScriptsJS/WebModerno/cap_ajax/server.js
+++ ScriptsJS/WebModerno/cap_ajax/server.js
@@ -7,8 +7,31 @@ app.use(express.static('.'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.get('/teste', (req,res) => {
-   res.send('Ok');
+app.get('/teste', (req, res) => {
+    res.send('Ok');
 });
 
-app.listen(8080, console.log('Executando'));
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './upload');
+    },
+    filename: function (req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`);
+    }
+});
+
+const upload = multer({storage}).single('arquivo');
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if (err) {
+            return res.end("Ocorreu um erro.");
+        }
+
+        res.end("Concluído com sucesso!");
+    });
+});
+
+app.listen(3003, console.log('Executando'));
