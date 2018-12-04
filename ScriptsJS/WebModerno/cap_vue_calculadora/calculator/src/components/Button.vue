63312b2f074ec05ee12b7ba114875e0c61fc42a0
dmcardoso <qualquercoisa6568@gmail.com>
2018-12-03 22:29:39 -0200

    início do capítulo de vue

---
+++ ScriptsJS/WebModerno/cap_vue_calculadora/calculator/src/components/Button.vue
@@ -0,0 +1,13 @@
+<template>
+    <button class="button">
+        0
+    </button>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style>
+
+</style>
---
+++ ScriptsJS/WebModerno/cap_vue_calculadora/calculator/src/components/Display.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="display">
+        0
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style>
+
+</style>
