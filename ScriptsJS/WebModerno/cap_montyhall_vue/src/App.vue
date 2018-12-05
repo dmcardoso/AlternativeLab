c3ac077e5342146b3aaebf238602872742c5643c
dmcardoso <qualquercoisa6568@gmail.com>
2018-12-05 13:28:23 -0200

    inicio do problema de monty hall

---
+++ ScriptsJS/WebModerno/cap_montyhall_vue/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="app">
+        <h1>Problema de Monty Hall</h1>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'app',
+        components: {}
+    }
+</script>
+
+<style>
+    * {
+        box-sizing: border-box;
+        font-family: "Montserrat", sans-serif;
+    }
+
+    body {
+        color: white;
+        background: linear-gradient();
+    }
+
+    #app {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    #app h1 {
+        border: 1px solid #000;
+        background-color: #0004;
+        padding: 20px;
+        margin-bottom: 60px;
+    }
+</style>
