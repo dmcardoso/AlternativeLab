74ded3961369476fdceb0da385dfca2e3921cc07
dmcardoso <qualquercoisa6568@gmail.com>
2018-12-06 16:45:34 -0200

    datas

--- ScriptsPHP/dateinterval.php
+++ ScriptsPHP/dateinterval.php
@@ -38,13 +38,35 @@ try {
 
     echo "<br><pre>";
 
-    print_r($periodo);
+//    print_r($periodo);
 
     $mes = '02';      // Mês desejado, pode ser por ser obtido por POST, GET, etc.
     $ano = date("Y"); // Ano atual
-    $ultimo_dia = date("t", mktime(0,0,0,$mes,'01','2012')); // Mágica, plim!
+    $ultimo_dia = date("t", mktime(0, 0, 0, $mes, "01", '2013')); // Mágica, plim!
     print_r($ultimo_dia);
 
+    // Mais 30 dias
+    $time = (30 * 24 * 60 * 60) + strtotime(date("Y-m-d"));
+    echo "<br>" . $time . "<br>";
+    echo date("Y-m-d", $time) . "<br>";
+
+    $mes_1 = "01";
+    $mes_2 = "02";
+    $mes_3 = "03";
+    $mes_4 = "04";
+    $mes_5 = "05";
+    $mes_6 = "06";
+    $mes_7 = "07";
+    $mes_8 = "08";
+    $mes_9 = "09";
+    $mes_10 = "11";
+    $mes_11 = "11";
+    $mes_12 = "12";
+
+    $bimestre = ceil((int)$mes_5 / 2);
+    echo "<br>" . $bimestre . "<br><br>";
+
+    echo(date("Y-m-d"));
 } catch (Exception $e) {
     echo $e;
 }
