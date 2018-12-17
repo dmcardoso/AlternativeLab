94e6d9413c21e409d490ec858cbf2044131ae96b
dmcardoso <qualquercoisa6568@gmail.com>
2018-12-17 17:31:09 -0200

    json2csv

---
+++ ScriptsJS/json2csv/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const Json2csvTransform = require('json2csv').Transform;
+
+const fields = ['nome', 'idade', 'turma'];
+const opts = { fields };
+const transformOpts = { highWaterMark: 16384, encoding: 'utf-8' };
+
+const input = fs.createReadStream('./data.json', { encoding: 'utf8' });
+const output = fs.createWriteStream('out.csv', { encoding: 'utf8' });
+const json2csv = new Json2csvTransform(opts, transformOpts);
+
+const processor = input.pipe(json2csv).pipe(output);
+
+// You can also listen for events on the conversion and see how the header or the lines are coming out.
+json2csv
+    .on('header', header => console.log(header))
+    .on('line', line => console.log(line))
+    .on('error', err => console.log(err));
