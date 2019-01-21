4ca1fe85958e0aaf58a3a99a142d83b475d74714
dmcardoso <qualquercoisa6568@gmail.com>
2019-01-21 13:57:08 -0200

    finalmente carai

--- ScriptsJS/Classes/file_watch/file_watch.js
+++ ScriptsJS/Classes/file_watch/file_watch.js
@@ -2,19 +2,51 @@ const fs = require('fs');
 const path = require('path');
 
 // Zera o conteúdo do arquivo
-fs.writeFile('./log.txt', '', (err) => {
-    if (err) throw err;
-    console.log('The file has been saved!');
-});
+// fs.writeFile('./log.txt', '', (err) => {
+//     if (err) throw err;
+//     console.log('The file has been saved!');
+// });
 
-let data_file_length = 0;
 
-fs.watchFile('./log.txt', (curr, prev) => {
-    // console.log(curr);
-    // console.log(prev);
-    fs.readFile('./log.txt', 'utf8', (err, data) => {
-        if (err) throw err;
-        console.log(data.substr(data_file_length));
-        data_file_length = data.length;
+// this.data_file_length = 0;
+//
+// fs.watch(path.resolve(__dirname, 'log.txt'), function (event) {
+//
+//     if (event === 'change') {
+//         fs.readFile(path.resolve(__dirname, 'log.txt'), 'utf8', (err, data) => {
+//             if (err) throw err;
+//                 const inicioString = this.data_file_length || 0;
+//                 this.data_file_length = data.length;
+//                 console.log(data.substring(inicioString, data.length));
+//                 console.log(inicioString);
+//                 console.log(this.data_file_length + "<");
+//         });
+//     }
+//
+// });
+
+const watch = require('node-watch');
+const readCache = require('read-cache');
+
+let contador = 0;
+
+const read = () => {
+    readCache(path.resolve(__dirname, 'log.txt')).then(function (contents) {
+        contents = contents.toString();
+        console.log(contents.substr(contador, contents.length));
+        contador = contents.length;
     });
+};
+
+
+const watcher = watch(path.resolve(__dirname, 'log.txt'), {recursive: true}, function (evt, name) {
+    read();
+    if (evt === "update") {
+        read();
+    }
 });
+
+// watcher.close();
+
+
+
