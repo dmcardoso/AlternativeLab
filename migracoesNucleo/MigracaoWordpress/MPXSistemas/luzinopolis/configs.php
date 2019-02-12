408c5446142917f1a7088706ff62afe5aea4c7b0
dmcardoso <qualquercoisa6568@gmail.com>
2019-02-12 14:27:24 -0200

    Migração de Luzinópolis e current path

---
+++ migracoesNucleo/MigracaoWordpress/MPXSistemas/luzinopolis/configs.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Daniel
+ * Date: 12/02/2019
+ * Time: 10:20
+ */
+
+const DBORIGEM = 'mpx_luzinopolis';
+const DBDESTINO = 'nucleoweb_luzinopolis';
+
+const PREFIXO = 'ng_';
+const IDGALLERY = 3098;
+
+//const SITE = '';
