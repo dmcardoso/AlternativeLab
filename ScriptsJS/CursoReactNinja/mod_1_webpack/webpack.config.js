6e2a39120091d00a71b7ae10452f0948361cb0cf
dmcardoso <qualquercoisa6568@gmail.com>
2019-02-26 15:22:59 -0300

    início ao curso de react ninja

---
+++ ScriptsJS/CursoReactNinja/mod_1_webpack/webpack.config.js
@@ -0,0 +1,10 @@
+'use stric';
+const path = require('path');
+
+module.exports = {
+    entry: path.join(__dirname, 'src', 'index'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    }
+};
