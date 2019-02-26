60770ad1ab0480393e174458340363ecfc9267fb
dmcardoso <qualquercoisa6568@gmail.com>
2019-02-26 17:02:08 -0300

    lista das aulas por pasta

---
+++ ScriptsJS/readdirlist.js
@@ -0,0 +1,68 @@
+const fs = require('fs');
+
+const path = "E:\\Daniel\\Cursos Udemy\\Curso React.js Ninja - React Completo";
+
+const initials = ["[M1]", "[MWP]", "[M1C]", "[M2]", "[M3]", "[M4]"];
+
+const directories = {
+    "[M1]": [],
+    "[MWP]": [],
+    "[M1C]": [],
+    "[M2]": [],
+    "[M3]": [],
+    "[M4]": []
+};
+
+const readDir = () => {
+    const folders = fs.readdirSync(path);
+
+    folders.forEach((value, index) => {
+        const files = fs.readdirSync(path + `\\${value}`);
+
+        files.forEach((video, i) => {
+            if (video.split('.vt')[1]) {
+                // legenda
+            } else {
+                directories[initials[index]].push(video.split('.mp4')[0]);
+            }
+        });
+    });
+
+};
+
+function start() {
+    readDir();
+
+    let stringTemplate = ``;
+    Object.keys(directories).forEach((cap, index) => {
+        stringTemplate += `- [] ${cap}\n`;
+
+        const numbers = directories[cap].map((value, index) => {
+            return Number(value.split('.')[0]);
+        });
+
+        const numbersWithIndex = directories[cap].map((value, index) => {
+            return [Number(value.split('.')[0]), index];
+        });
+
+        const ordered = numbers.sort(compararNumeros);
+
+        ordered.forEach((value, index) => {
+            numbersWithIndex.forEach((lecture, idx) => {
+                if (value === lecture[0]) stringTemplate += `\t - [] ${directories[cap][lecture[1]]}\n`;
+            });
+        });
+
+    });
+
+
+    fs.writeFile(__dirname + '/aulas.txt', stringTemplate, erro => {
+        console.log(erro || "Arquivo salvo");
+    });
+}
+
+function compareNumbers(a, b) {
+    return a - b;
+}
+
+start();
