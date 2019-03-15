b0bfc6d078e1f136e1b751bc0f1671fea75383f1
dmcardoso <qualquercoisa6568@hotmail.com>
2019-03-15 09:03:41 -0300

    Finaliza os cálculos para posições nas imagens

--- ScriptsJS/ImageManipulation/src/imagePosition.js
+++ ScriptsJS/ImageManipulation/src/imagePosition.js
@@ -7,12 +7,53 @@ const calculateImageCenter = ({imageWidth, imageHeight, markWidth, markHeight}) 
 
 const calculateCenter = ({imageSize, markSize}) => ((imageSize - markSize) / 2);
 
-const center = (dimensions = {}) => {
-    return calculateImageCenter(dimensions);
+const center = (imageWidth, imageHeight, markWidth, markHeight) => {
+    return calculateImageCenter({imageWidth, imageHeight, markWidth, markHeight});
 };
 
-const calculateImageLeft = ({markWidth, markHeight, marginTop, margin}) => {
+const calculateImageLeftBottom = ({imageHeight, markHeight, marginY, marginX}) => {
+    const x = marginX;
+    const y = imageHeight - (markHeight + marginY);
 
+    return {x, y};
 };
 
-module.exports = {center};
+const calculateImageLeftTop = ({marginX, marginY}) => {
+    const x = marginX;
+    const y = marginY;
+
+    return {x, y};
+};
+
+const calculateImageRightBottom = ({imageWidth, imageHeight, markWidth, markHeight, marginY, marginX}) => {
+    const x = imageWidth - markWidth - marginX;
+    const y = imageHeight - (markHeight + marginY);
+
+    return {x, y};
+};
+
+const calculateImageRightTop = ({imageWidth, markWidth, marginY, marginX}) => {
+    const x = imageWidth - markWidth - marginX;
+    const y = marginY;
+
+    return {x, y};
+};
+
+const left = ({marginX = 0, marginY = 0, imageHeight = 0, markHeight = 0, top = false}) => {
+    if (top) {
+        return calculateImageLeftTop({marginX, marginY});
+    } else {
+        return calculateImageLeftBottom({imageHeight, markHeight, marginY, marginX});
+    }
+};
+
+
+const rigth = ({marginX = 0, marginY = 0, imageWidth = 0, markWidth = 0, imageHeight = 0, markHeight = 0, top = false}) => {
+    if (top) {
+        return calculateImageRightTop({imageWidth, markWidth, marginY, marginX});
+    } else {
+        return calculateImageRightBottom({imageWidth, imageHeight, markWidth, markHeight, marginY, marginX});
+    }
+};
+
+module.exports = {center, left, rigth};
