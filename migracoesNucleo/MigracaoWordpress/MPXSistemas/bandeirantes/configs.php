97e5eeb6eb22e008a2f1c511a7705e0d6c7c19f5
dmcardoso <qualquercoisa6568@hotmail.com>
2019-03-26 16:45:43 -0300

    adiciona o script de migração de bandeirantes

---
+++ migracoesNucleo/MigracaoWordpress/MPXSistemas/bandeirantes/configs.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Daniel
+ * Date: 12/02/2019
+ * Time: 10:20
+ */
+
+const DBORIGEM = 'mpx_bandeirantes';
+const DBDESTINO = 'padrao_bandeirantes';
+
+const PREFIXO = 'ng_';
+const IDGALLERY = 2922;
+
+//const SITE = '';
