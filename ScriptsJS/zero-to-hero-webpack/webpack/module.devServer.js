4370035fbd968c696e3d1faa1b1e3248f729a716
dmcardoso <qualquercoisa6568@hotmail.com>
2019-06-27 14:11:46 -0300

    configurações do dev server

--- ScriptsJS/zero-to-hero-webpack/webpack/module.devServer.js
+++ ScriptsJS/zero-to-hero-webpack/webpack/module.devServer.js
@@ -4,6 +4,13 @@ const path = require('path');
 module.exports = (env, args) => (
     {
         contentBase: path.join('./', output.outputPath),
-        port: 9000
+        port: 9000,
+        // index: 'index.html',
+        // liveReload: false,
+        // open: true, || open: 'Google Chrome'
+        // overlay: true,
+        // proxy: {
+        //     '/api': 'http://localhost:3000'
+        // },
     }
 );
